a7c2cc75966613607dde7bfb8191b14fb7c7c885
AleksHolland84 <43878016+AleksHolland84@users.noreply.github.com>
2023-02-05 20:01:00 +0100

    Update app.py

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ if __name__ == "__main__":
         )
 
     date = st.date_input ("Dato", date(2023, 5, 2))
-    date.strftime("%d.%m.%Y")
+    date.strptime(date, "%Y/%m/%d").strftime("%d.%m.%Y")
         
         
     if class_size > 0:
